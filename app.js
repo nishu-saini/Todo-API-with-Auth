64bd270da2cee97abee839d719738ad7be2d79fd
quarnstric <nishusainialwar@gmail.com>
2023-07-19 18:08:57 +0530

    modified cors parameter

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ config({
 // Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Using Routes
 app.use("/api/v1/user", userRouter);
