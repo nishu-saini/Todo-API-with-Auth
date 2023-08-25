c1d2750fddb7232c2ebec369b3bb7c8728524be1
quarnstric <nishusainialwar@gmail.com>
2023-08-25 22:28:53 +0530

    Created Reducer added

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ config({
 // Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
+
+const frontendURL = process.env.FRONTEND_URL || "http://127.0.0.1:5173";
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [frontendURL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
